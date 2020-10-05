f292ea1bc8bddde3802dfb7fa29ca52d286eef61
Guylian Gilsing (School Laptop) <78902@roc-teraa.nl>
2020-10-05 09:40:28 +0200

    Simple base application

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace CsMarchingSquares
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.ReadLine();
+        }
+    }
+}
