6f91a33c11bb562ac5ffcdf49416db3fc1b53946
= <gilsingguylian@gmail.com>
2020-10-12 16:03:39 +0200

    Added a loop that refreshes the points and runs the algorithm over them

--- Form1.cs
+++ Form1.cs
@@ -12,9 +12,11 @@ namespace CsMarchingSquares
         private PictureBox pcbCanvas = null;
         private int pointDistance = 32;
         private MsqAlgorithm marchingSquaresAlgorithm = null;
-        private MsqRenderer renderer = null;
+        private MsqRenderer renderer = new MsqRenderer();
 
         private List<MsqPoint> debugPoints = new List<MsqPoint>();
+
+        Timer tmrUpdateCanvas = new Timer();
 
         public Form1()
         {
@@ -49,10 +51,21 @@ namespace CsMarchingSquares
         {
             this.marchingSquaresAlgorithm = new MsqAlgorithm(this.Width, this.Height, 32);
 
+            this.Update(null, null);
+
+            this.tmrUpdateCanvas.Interval = 500;
+            this.tmrUpdateCanvas.Tick += this.Update;
+            this.tmrUpdateCanvas.Start();
+        }
+
+        private void Update(Object a_sender, EventArgs a_e)
+        {
             // Generate and set the algorithm points
             List<MsqPoint> algoPoints = this.GeneratePoints(1, 0);
             this.marchingSquaresAlgorithm.SetPoints(algoPoints, 1, 0);
             this.marchingSquaresAlgorithm.RunOnce();
+        
+            this.pcbCanvas.Refresh();
         }
 
         /// <summary>
@@ -85,104 +98,7 @@ namespace CsMarchingSquares
 
         private void Draw(Object a_sender, PaintEventArgs a_e)
         {
-            if(this.renderer == null)
-                this.renderer = new MsqRenderer(a_e);
-
-            this.renderer.Render(this.marchingSquaresAlgorithm);
-
-            // this.DrawSquares(a_e);
-            // this.DrawPoints(a_e);
-            // this.DebugDraw(a_e);
+            this.renderer.Render(this.marchingSquaresAlgorithm, a_e);
         }
-
-        private void DrawSquares(PaintEventArgs a_e)
-        {
-            Brush brush = new SolidBrush(Color.Black);
-            Pen pen = new Pen(brush, 1);
-
-            foreach(MsqSquare square in this.marchingSquaresAlgorithm.squaresToMarch)
-            {
-                // Console.WriteLine($"tl: {square.topLeft.x} br: {square.bottomRight.x} w: {square.width} h: {square.height}");
-
-                pen.Color = Color.Black;
-                a_e.Graphics.FillRectangle(brush, new Rectangle(square.topLeft.x, square.topLeft.y, square.width - 1, square.height - 1));
-            }
-        }
-
-        #region ACTUAL_DEBUG_CODE
-
-        private void DrawSquare(int a_index, PaintEventArgs a_e)
-        {
-            Brush brush = new SolidBrush(Color.Black);
-            Pen pen = new Pen(brush, 1);
-
-            MsqSquare square = this.marchingSquaresAlgorithm.squaresToMarch[a_index];
-
-            pen.Color = Color.Black;
-            a_e.Graphics.FillRectangle(brush, new Rectangle(square.topLeft.x, square.topLeft.y, square.width - 1, square.height - 1));
-        }
-
-        #endregion
-
-        private void DrawPoints(PaintEventArgs a_e)
-        {
-            Brush brush = new SolidBrush(Color.Black);
-            Pen pen = new Pen(brush, 1);
-
-            foreach(MsqSquare square in this.marchingSquaresAlgorithm.squaresToMarch)
-            {
-                MsqPoint topLeft = new MsqPoint(square.topLeft.x, square.topLeft.y, square.topLeft.color);
-                MsqPoint topRight = new MsqPoint(square.topRight.x, square.topRight.y, square.topRight.color);
-                MsqPoint bottomLeft = new MsqPoint(square.bottomLeft.x, square.bottomLeft.y, square.bottomLeft.color);
-                MsqPoint bottomRight = new MsqPoint(square.bottomRight.x, square.bottomRight.y, square.bottomRight.color);
-
-                // Set the color and draw the corner points
-                pen.Color = Color.FromArgb(topLeft.color, topLeft.color, topLeft.color);
-                a_e.Graphics.DrawRectangle(pen, new Rectangle(topLeft.x, topLeft.y, 8, 8));
-
-                pen.Color = Color.FromArgb(topRight.color, topRight.color, topRight.color);
-                a_e.Graphics.DrawRectangle(pen, new Rectangle(topRight.x, topRight.y, 8, 8));
-
-                pen.Color = Color.FromArgb(bottomLeft.color, bottomLeft.color, bottomLeft.color);
-                a_e.Graphics.DrawRectangle(pen, new Rectangle(bottomLeft.x, bottomLeft.y, 8, 8));
-
-                pen.Color = Color.FromArgb(bottomRight.color, bottomRight.color, bottomRight.color);
-                a_e.Graphics.DrawRectangle(pen, new Rectangle(bottomRight.x, bottomRight.y, 8, 8));
-            }
-        }
-
-        #region DEBUG_CODE_REUSE_FOR_LATER_MAYBE
-
-        // private void DebugDraw(PaintEventArgs a_e)
-        // {
-        //     int pointAmountX = (this.Width / this.pointDistance);
-        //     int pointAmountY = (this.Height / this.pointDistance);
-
-        //     Console.WriteLine($"xa: {pointAmountX} ya: {pointAmountY}");
-
-        //     Brush brush = new SolidBrush(Color.Black);
-        //     Pen pen = new Pen(brush, 1);
-
-        //     for(int y = 0; y < pointAmountY + 2; y += 1)
-        //     {
-        //         for(int x = 0; x < pointAmountX + 2; x += 1)
-        //         {
-        //             if(x + 1 < pointAmountX && y + 1 < pointAmountY)
-        //             {
-        //                 int topLeftIndex = x + (y * pointAmountX);
-        //                 int topRightIndex = (x + 1) + (y * pointAmountX);
-        //                 int bottomLeftIndex = x + ((y + 1) * pointAmountX);
-        //                 int bottomRightIndex = (x + 1) + ((y + 1) * pointAmountX);
-
-        //                 MsqSquare square = new MsqSquare(this.debugPoints[topLeftIndex], this.debugPoints[topRightIndex], this.debugPoints[bottomLeftIndex], this.debugPoints[bottomRightIndex]);
-        //                 a_e.Graphics.FillRectangle(brush, new Rectangle(square.topLeft.x, square.topLeft.y, square.width, square.height));
-        //             }
-        //         }
-        //     }
-
-
-        // }
-
-        #endregion
     }
 }
--- GuylianGilsing/MarchingSquares/MsqRenderer.cs
+++ GuylianGilsing/MarchingSquares/MsqRenderer.cs
@@ -9,28 +9,24 @@ namespace GuylianGilsing.MarchingSquares
         private Brush brush = new SolidBrush(Color.White);
         private Pen pen = null;
 
-        public MsqRenderer(PaintEventArgs a_eventArgs)
-        {
-            this.graphics = a_eventArgs.Graphics;
-            this.pen = new Pen(this.brush, 1);
-        }
-
-        public void Render(MsqAlgorithm a_algo)
+        public void Render(MsqAlgorithm a_algo, PaintEventArgs a_e)
         {
             if(a_algo.squaresToMarch.Count > 0)
             {
                 foreach(MsqSquare square in a_algo.squaresToMarch)
                 {
-                    this.DrawState(square);
+                    this.DrawState(square, a_e);
                 }
             }
         }
 
-        private void DrawState(MsqSquare a_square)
+        private void DrawState(MsqSquare a_square, PaintEventArgs a_e)
         {
             int halfHeight = a_square.height / 2;
             int halfWidth = a_square.width / 2;
-
+            
+            Pen pen = new Pen(this.brush);
+            
             switch(a_square.state)
             {
                 case 0:
@@ -38,125 +34,125 @@ namespace GuylianGilsing.MarchingSquares
                 break;
                 
                 case 1:
-                    this.graphics.DrawLine(
-                        this.pen,
+                    a_e.Graphics.DrawLine(
+                        pen,
                         a_square.topLeft.x, a_square.topLeft.y + halfHeight,
                         a_square.bottomLeft.x + halfWidth, a_square.bottomLeft.y
                     );
                 break;
 
                 case 2:
-                    this.graphics.DrawLine(
-                        this.pen,
+                    a_e.Graphics.DrawLine(
+                        pen,
                         a_square.topRight.x, a_square.topRight.y + halfHeight,
                         a_square.bottomLeft.x + halfWidth, a_square.bottomRight.y
                     );
                 break;
 
                 case 3:
-                    this.graphics.DrawLine(
-                        this.pen,
+                    a_e.Graphics.DrawLine(
+                        pen,
                         a_square.topLeft.x, a_square.topLeft.y + halfHeight,
                         a_square.topRight.x, a_square.topRight.y + halfHeight
                     );
                 break;
 
                 case 4:
-                    this.graphics.DrawLine(
-                        this.pen,
+                    a_e.Graphics.DrawLine(
+                        pen,
                         a_square.topLeft.x + halfWidth, a_square.topLeft.y,
                         a_square.topRight.x, a_square.topLeft.y + halfHeight
                     );
                 break;
 
                 case 5:
-                    this.graphics.DrawLine(
-                        this.pen,
+                    a_e.Graphics.DrawLine(
+                        pen,
                         a_square.topLeft.x + halfWidth, a_square.topLeft.y,
                         a_square.topLeft.x, a_square.topLeft.y + halfHeight
                     );
 
-                    this.graphics.DrawLine(
-                        this.pen,
+                    a_e.Graphics.DrawLine(
+                        pen,
                         a_square.bottomLeft.x + halfWidth, a_square.bottomLeft.y,
                         a_square.bottomRight.x, a_square.topLeft.y + halfHeight
                     );
                 break;
 
                 case 6:
-                    this.graphics.DrawLine(
-                        this.pen,
+                    a_e.Graphics.DrawLine(
+                        pen,
                         a_square.topLeft.x + halfWidth, a_square.topLeft.y,
                         a_square.topLeft.x + halfWidth, a_square.bottomLeft.y
                     );
                 break;
 
                 case 7:
-                    this.graphics.DrawLine(
-                        this.pen,
+                    a_e.Graphics.DrawLine(
+                        pen,
                         a_square.topLeft.x + halfWidth, a_square.topLeft.y,
                         a_square.topLeft.x, a_square.topLeft.y + halfHeight
                     );
                 break;
 
                 case 8:
-                    this.graphics.DrawLine(
-                        this.pen,
+                    a_e.Graphics.DrawLine(
+                        pen,
                         a_square.topLeft.x + halfWidth, a_square.topLeft.y,
                         a_square.topLeft.x, a_square.topLeft.y + halfHeight
                     );
                 break;
 
                 case 9:
-                    this.graphics.DrawLine(
-                        this.pen,
+                    a_e.Graphics.DrawLine(
+                        pen,
                         a_square.topLeft.x + halfWidth, a_square.topLeft.y,
                         a_square.topLeft.x + halfWidth, a_square.bottomLeft.y
                     );
                 break;
 
                 case 10:
-                    this.graphics.DrawLine(
-                        this.pen,
+                    a_e.Graphics.DrawLine(
+                        pen,
                         a_square.topLeft.x + halfWidth, a_square.topLeft.y,
                         a_square.bottomRight.x, a_square.topLeft.y + halfHeight
                     );
 
                     // Posible bug here...
-                    this.graphics.DrawLine(
-                        this.pen,
+                    a_e.Graphics.DrawLine(
+                        pen,
                         a_square.topLeft.x, a_square.topLeft.y + halfHeight,
                         a_square.bottomLeft.x  + halfWidth, a_square.bottomLeft.y
                     );
                 break;
 
                 case 11:
-                    this.graphics.DrawLine(
-                        this.pen,
+                    a_e.Graphics.DrawLine(
+                        pen,
                         a_square.topLeft.x + halfWidth, a_square.topLeft.y,
                         a_square.bottomRight.x, a_square.topLeft.y + halfHeight
                     );
                 break;
 
                 case 12:
-                    this.graphics.DrawLine(
-                        this.pen,
+                    a_e.Graphics.DrawLine(
+                        pen,
                         a_square.topLeft.x, a_square.topLeft.y + halfHeight,
                         a_square.topRight.x, a_square.topRight.y + halfHeight
                     );
                 break;
 
                 case 13:
-                    this.graphics.DrawLine(
-                        this.pen,
+                    a_e.Graphics.DrawLine(
+                        pen,
                         a_square.bottomLeft.x + halfWidth, a_square.bottomLeft.y,
                         a_square.bottomRight.x, a_square.topLeft.y + halfHeight
                     );
                 break;
 
                 case 14:
-                    this.graphics.DrawLine(
-                        this.pen,
+                    a_e.Graphics.DrawLine(
+                        pen,
                         a_square.topLeft.x, a_square.topLeft.y + halfHeight,
                         a_square.bottomLeft.x  + halfWidth, a_square.bottomLeft.y
                     );
